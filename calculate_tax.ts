48c1bd564a7e0c1021ae6695bca3eac21827fd43
Malav Soni <malav@live.com>
2025-01-26 17:54:50 +0530

    dfs logic added

--- calculate_tax.ts
+++ calculate_tax.ts
@@ -3,6 +3,7 @@ function calculateTax(brackets: number[][], income: number): number {
   let remainingIncome: number = income;
 
   for (let i = 0; i < brackets.length; i++) {
+    // O(n)
     let range = 0;
     if (i > 0) {
       range = brackets[i][0] - brackets[i - 1][0];
@@ -17,10 +18,45 @@ function calculateTax(brackets: number[][], income: number): number {
   return tax;
 }
 
+function calculateTax_dfs(brackets: number[][], income: number): number {
+  function dfs(income: number, i: number): number {
+    if (income <= 0) {
+      return 0;
+    }
+
+    let tax: number = 0;
+    let remainingIncome: number = income;
+
+    let range = 0;
+    if (i > 0) {
+      range = brackets[i][0] - brackets[i - 1][0];
+    } else {
+      range = brackets[i][0];
+    }
+    let taxableAmount = Math.min(remainingIncome, range);
+    tax += (taxableAmount * brackets[i][1]) / 100;
+    remainingIncome -= taxableAmount;
+
+    return tax + dfs(remainingIncome, i + 1);
+  }
+  return dfs(income, 0);
+}
+
 describe("2303. Calculate Amount Paid in Taxes", () => {
   it("Happy Path - 01", () => {
     expect(
       calculateTax(
+        [
+          [3, 50],
+          [7, 10],
+          [12, 25],
+        ],
+        10
+      )
+    ).toStrictEqual(2.65);
+
+    expect(
+      calculateTax_dfs(
         [
           [3, 50],
           [7, 10],
